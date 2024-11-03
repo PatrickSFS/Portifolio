73b0d457e7840347f167dcf83f6fb64b6226ce35
PatrickSFS <patrick.freitas@engenharia.ufjf.br>
2024-11-03 12:26:24 -0300

    add: animation in CARDF

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,17 +3,17 @@ import PropTypes from 'prop-types';
 
 CardF.propTypes = {
   text: PropTypes.string.isRequired,
-  title:PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
   buttonContent: PropTypes.string.isRequired,
   icon: PropTypes.string,
 };
 
 function CardF(props) {
   return (
-    <Card className='bg-neutral-900' style={{ width: '18rem' }}>
-    <i className={`${props.icon} text-white size-3 p-2`}></i>
+    <Card className='bg-neutral-900 transform transition-transform duration-200 hover:-translate-y-1 hover:scale-105 shadow-lg hover:shadow-xl cursor-pointer' style={{ width: '18rem' }}>
+      <i className={`${props.icon} text-white size-3 p-2`}></i>
       <Card.Body>
-        <Card.Title className='text-orange-500 opacity-90 '>{props.title}</Card.Title>
+        <Card.Title className='text-orange-500 opacity-90'>{props.title}</Card.Title>
         <Card.Text className='text-white opacity-80 text-justify'>
           {props.text}
         </Card.Text>
